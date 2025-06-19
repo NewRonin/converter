eb1502b1e15d533cf78ea609f0d0cd412433ac5c
Maxim Dudarev <max.md8@gmail.com>
2025-06-19 03:46:41 +0300

    (pages/index.vue): added main page

---
+++ components/common/Table.vue
@@ -0,0 +1,47 @@
+<template>
+  <table class="table-container">
+    <thead>
+      <tr>
+        <th v-for="header in headers">{{ header.name }}</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(row, rowIndex) in rows" :key="rowIndex">
+        <td v-for="key in keys" :key="key">{{ row[key] }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script setup lang="ts">
+
+type Headers = {
+  name: string,
+  value: string,
+}
+
+const props = defineProps<{
+  headers: Headers[];
+  rows: Record<string, any>[];
+}>();
+
+const keys = computed(() => props.headers.map(x => x.value));
+</script>
+
+<style scoped>
+.table-container {
+  width: 100%;
+  border-collapse: collapse;
+}
+
+.table-container th,
+.table-container td {
+  border: 1px solid var(--border);
+  padding: 0.5rem 1rem;
+  text-align: left;
+}
+
+.table-container thead {
+  background-color: var(--primary-light);
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,72 @@
 <template>
-
+  <div class="page-container">
+    <main>
+      <div class="title-container">
+        {{ `Курсы валют к ${store.selectedCurrency.toUpperCase()}` }}
+      </div>
+      <Table
+        v-if="!store.loading"
+        :headers="headers"
+        :rows="rows"
+        class="currency-table"
+      />
+    </main>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { useMainStore } from "@/stores/main";
+import Table from "@/components/common/Table.vue";
 
-const store = useMainStore()
+const store = useMainStore();
+store.getCurrency();
 
-store.getCurrency()
+const headers = [
+  { name: "Валюта", value: "currency" },
+  { name: "Курс", value: "rate" },
+];
 
+const rows = computed(() => {
+  return Object.entries(store.rates)
+    .filter(([pair]) => pair.endsWith(`-${store.selectedCurrency}`))
+    .map(([pair, rate]) => {
+      const currency = pair.split("-")[0].toUpperCase();
+      return { currency, rate };
+    });
+});
 </script>
+
+<style scoped lang="scss">
+.page-container {
+  position: relative;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  padding-top: 5.6rem;
+
+  main {
+    width: 100%;
+    display: flex;
+    flex-flow: column nowrap;
+    justify-content: flex-start;
+    align-items: center;
+    gap: 10rem;
+
+    padding: 4rem;
+
+    .currency-table {
+      max-width: 120rem;
+      font-size: 1.6rem;
+    }
+
+    .title-container{
+        width: 100%;
+        max-width: 120rem;
+        font-size: 2rem;
+        text-align: left;
+    }
+  }
+}
+</style>
